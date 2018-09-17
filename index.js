1184f63aa2e75b75ac68f6f86c9831d134c930a1
Jeremy Clarke <jeremy@savetrees.co.uk>
2018-09-17 22:26:08 +0100

    Added .pug template file compilation, updated readme and package.json

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-module.exports = (api) => {
+module.exports = api => {
   api.chainWebpack(webpackConfig => {
+    // Remove any existing rule added from a previous version of the plugin (npm uninstall/ yarn remove will remove the plugin, but leave behind the webpack rules)
+    webpackConfig.module.rules.delete('pug')
+
+    // Rules taken from: https://vue-loader.vuejs.org/guide/pre-processors.html#pug
     webpackConfig.module
-        .rule('pug')
+      .rule('pug')
         .test(/\.pug$/)
-        .use('pug-plain-loader')
-        .loader('pug-plain-loader')
-        .end()   
+
+        // this applies to <template lang="pug"> in Vue components
+        .oneOf('vue-loader')
+          .resourceQuery(/^\?vue/)
+          .use('pug-plain')
+            .loader('pug-plain-loader')
+            .end()
+        .end()
+
+        // this applies to pug imports inside JavaScript, i.e. .pug files
+        .oneOf('raw-pug-files')
+          .use('pug-raw')
+            .loader('raw-loader')
+            .end()
+          .use('pug-plain')
+            .loader('pug-plain-loader')
+            .end()
+        .end()
   })
 }
--- readme.md
+++ readme.md
@@ -1,17 +1,30 @@
 # vue-cli-plugin-pug
 
 [![npm version](https://badge.fury.io/js/vue-cli-plugin-pug.svg)](https://badge.fury.io/js/vue-cli-plugin-pug)
-Vue CLI 3 plugin to add pug templating to your components.
+Vue CLI 3 plugin to add pug templating to your components and compilation of .pug template files
 
 ## Getting Started
-Just run the following in your Vue-cli 3 project: 
+Either add the plugin from the `vue ui` interface, or run the following in your Vue-cli 3 project: 
 ```sh
 vue add pug
 ```
-You can now start using pug template in your components:
+You can now start using pug templates in your components:
 ```html
 <template lang='pug'>
   div
     h1 Hello World!
 </template>
+```
+
+The plugin will also convert `.pug` files to html, so you can add entries to `vue.config.js` and get a (build) output file of `./dist/public/index.html`:
+
+```js
+module.exports = {
+  pages: {
+    index: {
+      entry: './src/main.js',
+      template: './src/index.pug'
+    }
+  }
+}
 ```
